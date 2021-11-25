0b6c9f766279f22e4a576fe0c4be088a98408455
RachanaS79 <rachana.s.7997@gmail.com>
2021-11-25 12:03:27 +0530

    peakElement modified

--- src/com/datastructures/array/PeakElement.java
+++ src/com/datastructures/array/PeakElement.java
@@ -2,18 +2,25 @@ package com.datastructures.array;
 
 public class PeakElement {
 
+    /*
+    Time Complexity: O(Logn).
+    Where n is the number of elements in the input array. In each step our search becomes half. So it can be compared to Binary search, So the time complexity is O(log n)
+    Space complexity: O(1).
+    No extra space is required, so the space complexity is constant.
+     */
     static int findPeak(int arr[], int low, int high, int n){
 
-        int mid = (low + high)/2;
+        int mid = low + (high - low)/2;
 
         if((mid == 0 || arr[mid -1] <= arr[mid]) && (mid == n-1 || arr[mid +1] <= arr[mid]))
         return mid;
 
-        else if (mid > 0 && arr[mid -1] > arr[mid])
-           return findPeak(arr, low, (mid -1), n);
-
+        // if mid is not peak element
+        else if (mid > 0 && arr[mid-1] > arr[mid]){
+            return findPeak(arr, low, mid-1, n);
+        }
         else
-            return findPeak(arr, (mid + 1), high, n);
+            return findPeak(arr, mid+1, high, n);
     }
 
     public static void main(String[] args){
